dd6a40fd71d5114489c4a1af576d6b1248b0a8cb
Manu1102 <manuelschmitzberger4@gmail.com>
2023-03-27 21:27:04 +0200

    Änderung_auf_Dhango

---
+++ second_try/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SecondTryConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'second_try'
---
+++ second_try/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def main(request):
+    return render(request,"index.html")
+
+def login(request):
+    return render(request,"login.html")
+
