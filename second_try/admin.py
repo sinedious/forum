b4247dd83f1a421ede4ce91c33350d933c6a2011
Manu1102 <manuelschmitzberger4@gmail.com>
2023-04-02 15:13:33 +0200

    alles neu

--- second_try/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from second_try.models import AccessRecord,Topic,Webpage,Users_test
-# Register your models here.
-admin.site.register(AccessRecord)
-admin.site.register(Topic)
-admin.site.register(Webpage)
-admin.site.register(Users_test)
--- second_try/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from second_try import views
-
-urlpatterns = [
-    path("",views.login,name='login'),
-]
