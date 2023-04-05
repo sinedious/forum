2c50e19793d664e88590d410c2700db486578631
Manu1102 <manuelschmitzberger4@gmail.com>
2023-04-05 15:06:27 +0200

    label entfernt und placeholder eingefügt

--- forum/forum_app/forms.py
+++ forum/forum_app/forms.py
@@ -10,5 +10,6 @@ class PostBeitrag(forms.ModelForm):
     class Meta:
         model = beitrag
         fields = ['titel', 'beitrag']
-        widgets = {'beitrag': forms.Textarea()}
+        widgets = {'beitrag': forms.Textarea(attrs={'placeholder': 'Beitrag'}), 
+                   'titel':forms.TextInput(attrs={'placeholder': 'Titel'})}
         
