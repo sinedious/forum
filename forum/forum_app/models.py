09673a6c9519f11c9580498a6020f12c4025b1c2
E0429C59DF <robin4231jani@gmail.com>
2023-05-13 16:18:33 +0200

    beitrag

--- forum/forum_app/models.py
+++ forum/forum_app/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class beitrag(models.Model):
     titel = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     beitrag = models.CharField(max_length=500)
-
-    def __str__(self) -> str:
-        return self.titel
+    def __str__(self):
+        return self.titel + ' | ' + str(self.author)
     
     
 
--- forum/forum_app/views.py
+++ forum/forum_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from forum_app import forms
+from .models  import beitrag
 
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -7,6 +8,7 @@ from django.urls import reverse
 
 def index(request):
     post = forms.PostBeitrag()
+    all_beitrag = beitrag.objects.all()
     welcome = request.user.username
 
     if request.method == 'POST':
@@ -17,7 +19,7 @@ def index(request):
             post = forms.PostBeitrag()
             return HttpResponseRedirect(reverse('index'))
         
-    return render(request, 'forum_app/index.html',{'beitrag_form':post, 'username':welcome})
+    return render(request, 'forum_app/index.html',{'beitrag_form':post,'beitrag':all_beitrag, 'username':welcome})
 
 
 
